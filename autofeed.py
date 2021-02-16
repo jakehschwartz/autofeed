d2f555ed9ad45f956af64d0baf20f825d02b4380
Jake Schwartz <jakehschwartz@gmail.com>
2021-02-15 20:59:33 -0500

    Better printing

--- autofeed.py
+++ autofeed.py
@@ -1,32 +1,37 @@
+import datetime
 import feedparser
 import json
 import os
 
-from datetime import datetime
 from time import mktime
 
 # Get a list of feeds
 feeds = os.listdir('./feeds/')
 feed_map = {f[0:-4]: open(f'./feeds/{f}',).readlines() for f in feeds}
-print(feed_map)
+filtered_feed_map = {k:list(filter(None, list(map(str.strip, v)))) for k, v in feed_map.items()}
+print(filtered_feed_map)
 
 # Get the last saved time per set of feeds
-times = {key: datetime.fromisoformat(value) for key, value in json.load(open('times.json',)).items()}
-print(times)
+times = {key: datetime.datetime.fromisoformat(value) for key, value in json.load(open('times.json',)).items()}
 
 # Config a place to send things
 
+# Read the feeds
 new_times = {}
-for feed_type, feeds in feed_map.items():
-  now = datetime.utcnow().isoformat()
+for feed_type, feeds in filtered_feed_map.items():
+  print("==============")
+  print(feed_type)
+  now = datetime.datetime.utcnow()
+  time = times.get(feed_type, now - datetime.timedelta(days = 30))
   for feed in feeds:
     d = feedparser.parse(feed)
-    print(d.feed)
+    print(d.feed.title,':')
     for e in d.entries:
-      published_date = datetime.fromtimestamp(mktime(e.published_parsed))
-      if published_date > times[feed_type]:
+      published_date = datetime.datetime.fromtimestamp(mktime(e.published_parsed))
+      if published_date > time:
         print(f"{e.title}: {e.link} ({published_date.isoformat()})")
-  new_times[feed_type] = now
+    print("")
+  new_times[feed_type] = now.isoformat()
 
 
 # After checking everything, save time per list
