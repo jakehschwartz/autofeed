6c860e8f825cde503a1efbf27ab35f35a726bc4f
Jake Schwartz <jakehschwartz@gmail.com>
2021-02-16 19:59:10 -0500

    Move feeds file to HOME dir

--- autofeed.py
+++ autofeed.py
@@ -8,18 +8,19 @@ import sys
 
 from time import mktime
 from email.mime.text import MIMEText
+from pathlib import Path
 
 parser = argparse.ArgumentParser(description='Auto RSS Reader')
-parser.add_argument('--list', action='store_true', help='list all feed')
+parser.add_argument('--show', action='store_true', help='show all feeds')
 parser.add_argument('--gmail-username', help='username for gmail user')
 parser.add_argument('--gmail-password', help='password for gmail user')
 args = parser.parse_args()
 
 # Get a list of feeds
-feeds = os.listdir('./feeds/')
-feed_map = {f[0:-5]: json.load(open(f'./feeds/{f}',)) for f in feeds}
+file_location = f'{Path.home()}/.autofeed/feeds.json'
+feed_map = json.load(open(file_location))
 
-if args.list:
+if args.show:
     output = ""
     for key, entries in feed_map.items():
       output += f"{key}\n"
@@ -30,17 +31,18 @@ if args.list:
 else:
     # Read the feeds
     msgs = {}
+    new_feed_map = {}
     for feed_type, feeds in feed_map.items():
       now = datetime.datetime.utcnow()
       new_feeds = []
       article_map = {}
       for feed in feeds:
         d = feedparser.parse(feed['url'])
-        time = feed.get('last_read',now - datetime.timedelta(days = 30))
-        entries = [e for e in d.entries if datetime.datetime.fromtimestamp(mktime(e.published_parsed)) > time]
+        time = feed.get('last_read', (now - datetime.timedelta(days = 30)).isoformat())
+        entries = [e for e in d.entries if datetime.datetime.fromtimestamp(mktime(e.published_parsed)) > datetime.datetime.fromisoformat(time)]
         if len(entries):
           article_map[d.feed.title] = entries
-        new_feeds.append({'url':feed['url'], 'time':now.isoformat()})
+        new_feeds.append({'url':feed['url'], 'last_read':now.isoformat()})
 
       if len(article_map):
         email_body = ""
@@ -54,30 +56,35 @@ else:
         message = f"{email_body}Love,\nAutofeed"
 
         msgs[feed_type] = (subject, message) 
+        
+      new_feed_map[feed_type] = new_feeds  
 
-      # After checking everything, save time per list
-      with open(f"./feeds/{feed_type}.json", "w") as outfile:
-        json.dump(new_feeds, outfile)
+   # After checking everything, save feeds with new times
+    with open(file_location, "w") as outfile:
+      json.dump(new_feed_map, outfile)
 
-    if args.gmail_username and args.gmail_password:
-      sender = f'{args.gmail_username}@gmail.com'
-      print("Sending emails to", sender,args.gmail_password)
-      s = smtplib.SMTP_SSL(host = 'smtp.gmail.com', port = 465)
-      s.login(user = args.gmail_username, password = args.gmail_password)
-      s.ehlo()
-      for k, (subject, message) in msgs.items():
-        print("==============")
-        print(subject)
-        print(message)
-        msg = MIMEText(message, _charset="UTF-8")
-        msg['From'] = sender
-        msg['To'] = sender
-        msg['Subject'] = subject
-        s.sendmail(sender, sender, msg.as_string())
-      s.quit()
+    if len(msgs):
+      if args.gmail_username and args.gmail_password:
+        sender = f'{args.gmail_username}@gmail.com'
+        print("Sending emails to", sender,args.gmail_password)
+        s = smtplib.SMTP_SSL(host = 'smtp.gmail.com', port = 465)
+        s.login(user = args.gmail_username, password = args.gmail_password)
+        s.ehlo()
+        for k, (subject, message) in msgs.items():
+          print("==============")
+          print(subject)
+          print(message)
+          msg = MIMEText(message, _charset="UTF-8")
+          msg['From'] = sender
+          msg['To'] = sender
+          msg['Subject'] = subject
+          s.sendmail(sender, sender, msg.as_string())
+        s.quit()
+      else:
+        for k, (subject, message) in msgs.items():
+          print("==============")
+          print(subject)
+          print(message)
     else:
-      for k, (subject, message) in msgs.items():
-        print("==============")
-        print(subject)
-        print(message)
+        print("No new articles")
 
